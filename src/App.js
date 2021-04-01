fd78937e20f6e4abc5654380b84eb7abcc0ccbfc
GuilhermePellegrini <69095229+GuilhermePellegrini@users.noreply.github.com>
2021-03-31 22:18:56 -0300

    Cronometro
    Site simples de em react.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,114 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Cronometro from './assets/cronometro.png';
+import './style.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    
+    constructor(props) {
+        super(props);
+        this.state ={
+            timer: '00:00:00',
+            horas: {
+                hora: 0,
+                minutos: 0,
+                segundos: 0
+            },
+            btnIniciar: 'Iniciar',
+            salvo: [],
+            erro: ''
+        };
+
+        this.relogio = null;
+        this.iniciar = this.iniciar.bind(this);
+        this.zerar = this.zerar.bind(this);
+        this.salvar = this.salvar.bind(this);
+        this.zerarSalvos = this.zerarSalvos.bind(this);
+    }
+
+    //Iniciando Cronometro
+    iniciar(){
+
+        if(this.relogio !== null){
+            clearInterval(this.relogio);
+            this.relogio = null;
+            this.setState({btnIniciar: 'Iniciar'});
+        }else{
+            this.setState({btnIniciar: 'Pausar'});
+            this.relogio = setInterval(() => {
+                let state = this.state;
+                state.horas.segundos += 1;
+                if(state.horas.segundos === 60){
+                    state.horas.segundos = 0;
+                    state.horas.minutos += 1;
+                    if(state.horas.minutos === 60){
+                        state.horas.minutos = 0;
+                        state.horas.hora += 1;
+                    }
+                }
+                let hora = state.horas.hora;
+                let minutos = state.horas.minutos;
+                let segundos = state.horas.segundos;
+                if(hora < 10){
+                    hora = '0'+state.horas.hora;
+                }
+                if(minutos < 10){
+                    minutos = '0'+state.horas.minutos;
+                }
+                if(segundos < 10){
+                    segundos = '0'+state.horas.segundos;
+                }
+                state.timer = hora+":"+minutos+":"+segundos;
+                this.setState(state);
+            },1000)
+        }
+    }
+
+    zerar(){
+        if(this.relogio !== null){
+            this.iniciar();
+        }
+        this.setState({timer: '00:00:00'});
+        this.setState({horas: {hora: 0, minutos: 0, segundos: 0}});
+    }
+
+    salvar(){
+        let time = this.state.timer;
+        let tamanho = this.state.salvo.length;
+        if(tamanho >= 15){
+            this.setState({erro: "Numero maximo de tempos salvos atingido."});
+        }else{
+            this.setState({salvo: [...this.state.salvo, time]});
+        }
+    }
+
+    zerarSalvos(){
+        this.setState({salvo: []});
+        this.setState({erro: ""});
+    }
+
+    render(){
+        return(
+            <div className='container'>
+                <div className='erro'>{this.state.erro}</div>
+                <img src={Cronometro} alt='cronometro' className='img'/>
+                <a className='timer' href>{this.state.timer}</a>
+                <div className='areaBtn'>
+                    <a className='btn' href onClick={this.iniciar}>{this.state.btnIniciar}</a>
+                    <a className='btn' href onClick={this.zerar}>Zerar</a>
+                    <a className='btn' href onClick={this.salvar}>Salvar</a>
+                </div>
+                <div className='salvar'>
+                    <b>Salvos</b>
+                    {this.state.salvo.map(function(time){
+                        return (
+                            <div className='salvo'>{time}</div>
+                        );
+                    })}
+                    <a className='btnBlack' href onClick={this.zerarSalvos}>Zerar Salvos</a>
+                </div>
+            </div>
+        );
+    }
 }
 
-export default App;
+export default App
